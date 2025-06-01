404f4a9a1746531f1f426004299605d9829f4fee
biruk <biruk_ketema@yahoo.com>
2025-06-01 15:31:05 -0600

    Backend integrated

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 const axiosInstance = axios.create({
-	baseURL: 'http://localhost:5500'
+	// baseURL: 'http://localhost:5500'
+	baseURL: 'https://amazon-backend-mh4m.onrender.com'
 });
 export { axiosInstance };
